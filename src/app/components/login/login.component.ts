3f36c5d42724b70b941d2decbd9f548b7325d18d
huynguyen44 <huypokemon@gmail.com>
2020-01-31 13:50:08 -0600

    Adjusted Login
    Adjusted login components and register components, added some data validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,14 @@
 import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 
 @Component({
@@ -7,6 +17,16 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  usernameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ]);
+
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  matcher = new MyErrorStateMatcher();
 
   constructor() {
   }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,11 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  isSubmitted = false;
   Roles: any = ['Parent', 'Student'];
+  registrationForm = this.fb.group({
+    roleName: [''],
+    saintName: [''],
+    lastName: [''],
+    middleName: [''],
+    firstName: [''],
+    email: [''],
+    address: [''],
+    city: [''],
+    zipCode: ['']
+  });
 
-  constructor() { }
+  constructor(public fb: FormBuilder) {
+  }
 
-  ngOnInit() {
+  get roleName() {
+    return this.registrationForm.get('roleName');
+  }
+
+  onSubmit() {
+    alert(JSON.stringify(this.registrationForm.value));
+  }
+
+  ngOnInit(): void {
   }
 
 }
